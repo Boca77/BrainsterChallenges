7a57d081c6c2c06a740477d885a59763ab987e33
Boris Nacev <bnachev77@gmail.com>
2024-09-01 20:52:23 +0200

    final

--- database/migrations/2024_08_27_160915_add_post_id_to_comments_table.php
+++ database/migrations/2024_08_27_160915_add_post_id_to_comments_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('comments', function (Blueprint $table) {
-            $table->foreignId('post_id')->after('content')->constrained('posts');
+            $table->foreignId('post_id')->after('content')->constrained('posts')->onDelete('cascade');
         });
     }
 
